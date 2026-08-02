fed3b7487dcc9454f28e625bf402a1bb0ed2c704
Kyoze <hrmnfrmnsyh1210@gmail.com>
2026-08-02 19:16:21 +0700

    komit awal

---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2026-08-02',
+  devtools: { enabled: true }
+})
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <h1>ErgoSelf</h1>
+    <p>Nuxt 3 siap dipakai.</p>
+  </main>
+</template>
